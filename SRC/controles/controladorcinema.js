d7f8da7030b2d7e5ce5e86a215f63fcbea80a1b3
505 <505@academicos.cesde.edu.co>
2022-08-24 21:00:13 -0500

    repaso array

---
+++ SRC/controles/controladorcinema.js
@@ -0,0 +1,105 @@
+let peliculas=[
+    {
+        nombre:"BESTIA",
+        genero:"Ciencia ficcion",
+        duracion: 220,
+        imagenes:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/a1.jpg?alt=media&token=b2cf7252-7078-415b-87dc-fa44464e669a",
+        sinopsis:"A veces el susurro en los arbustos es en realidad un monstruo. Idris Elba (Rápidos y Furiosos: Hobbs & Shaw, El Escuadrón Suicida) protagoniza un nuevo y emocionante thriller sobre un padre y sus dos hijas adolescentes que se ven perseguidos por un enorme león que intenta demostrar que la selva sólo tiene un máximo depredador. Elba interpreta al Dr. Nate Daniels, un marido que acaba de enviudar y que regresa a Sudáfrica, donde conoció a su mujer, en un viaje que llevaba tiempo planeando con sus hijas a una zona de caza controlada por Martin Battles (Sharlto Copley, serie Muñeca Rusa, Maléfica), un viejo amigo de la familia y biólogo especializado en vida salvaje.",
+        clasificacion:"+18",
+        idioma:"En sutitulado"
+    },
+    {
+        nombre:"THOR:Love and Thunder",
+        genero:"Ciencia ficcion",
+        duracion: 260,
+        imagenes:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/a10.png?alt=media&token=132eafeb-3e87-4715-a18f-03f849983928",
+        sinopsis:"Thor: Love and Thunder de Marvel Studios muestra al dios del Trueno en un viaje diferente a todo lo que ha enfrentado: una búsqueda de la paz interior. Pero el retiro de Thor se ve interrumpido por un asesino galáctico conocido como Gorr el dios Carnicero, que busca la extinción de los dioses",
+        clasificacion:"General",
+        idioma:"Es"
+    },
+    {
+        nombre:"Minions",
+        genero:"Infantil",
+        duracion: 200,
+        imagenes:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/minions.jpg?alt=media&token=59d5f530-c3ba-4fb1-bf38-7a9a6d5fe362",
+        sinopsis:"En los años 70, Gru crece siendo un gran admirador de Los salvajes seis, un supergrupo de villanos. Para demostrarles que puede ser malvado, Gru idea un plan con la esperanza de formar parte de la banda. Por suerte, cuenta con la ayuda de sus fieles seguidores, los Minions, siempre dispuestos a sembrar el caos.",
+        clasificacion:"General",
+        idioma:"ES"
+    },
+    {
+        nombre:"Un parcero en Nueva York",
+        genero:"Comedia",
+        duracion: 100,
+        imagenes:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/UN%20PARCERO%20EN%20NUEVA%20YORK.png?alt=media&token=72abb0b6-6617-4fb4-90bd-f24f0b9de929",
+        sinopsis:"Armando Pulido, un trabajador y buen amigo, cansado de la grave crisis económica que vive en Colombia, decide ir en busca del sueño americano a Nueva York e intenta de diversas maneras conseguir la visa americana.",
+        clasificacion:"General",
+        idioma:"ES"
+    },
+    {
+        nombre:"Gemelo Siniestro",
+        genero:"Suspenso",
+        duracion: 200,
+        imagenes:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/a2.png?alt=media&token=dcbfbf72-a264-4382-b7bf-1044984a2684",
+        sinopsis:"Tras la muerte de uno de sus hijos gemelos, Rachel y Anthony se instalan en el campo escandinavo para sanar las heridas anímicas. Allí, sin embargo, ella descubre que una fuerza sobrenatural y maligna quiere llevarse al pequeño que le queda.",
+        clasificacion:"+18",
+        idioma:"EN"
+    },
+    { 
+         nombre:"Perro Samurai",
+         genero:"Infantil",
+         duracion: 180,
+         imagenes:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/a3.png?alt=media&token=284762c2-7966-4068-9ed2-d227751b455a",
+        sinopsis:"Un pueblo de gatos se encuentra en peligro y un héroe inesperado surge para ayudarlos: ¡un perro llamado Hank!",
+        clasificacion:"General",
+        idioma:"ES"
+    },
+    {
+        nombre:"Sana que Sana",
+        genero:"Comedia",
+        duracion: 160,
+        imagenes:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/a4.png?alt=media&token=b9660c44-dbb8-4d60-8783-13625dd64f3e",
+        sinopsis:"En una gran aventura en la que los súpercalvos se convierten en súper héroes, Rogelio logra enfrentar a los malos haciéndoles pagar su osadía e infamia de robarse la plata de la salud, a la que tienen derecho todos los niños colombianos por igual y sin distingo.",
+        clasificacion:"General",
+        idioma:"ES"
+    },
+    {
+        nombre:"Tren Bala",
+        genero:"Accion",
+        duracion: 126,
+        imagenes:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/trebala.jpg?alt=media&token=fd1461e9-ee71-48e1-ba52-f421280ee043",
+        sinopsis:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/trebala.jpg?alt=media&token=fd1461e9-ee71-48e1-ba52-f421280ee043",
+        clasificacion:"+18",
+        idioma:"ES"
+    },
+    {
+        nombre:"Dragon Ball Super: Super Hero",
+        genero:"Ciencia Ficcion",
+        duracion: 150 ,
+        imagenes:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/DRAGON.jpg?alt=media&token=2a8174f0-7d81-48b7-b874-f52a8780fc59",
+        sinopsis:"Son Goku destruyó en su momento a la Patrulla Roja. Ahora, ciertos individuos han decidido continuar con su legado y han creado a los androides definitivos: Gamma 1 y Gamma 2. Estos dos androides se autoproclaman superhéroes y deciden atacar a Piccolo y a Gohan. ¿Cuál es el objetivo de la nueva Patrulla Roja? Ante un peligro inminente, ¡llega el momento del despertar del Superhéroe! La película, que cuenta con el compromiso e intervención del creador original de Dragon Ball, Akira Toriyama, tiene al legendario creador de manga detrás de la historia original, el guión y el diseño de personajes del filme.",
+        clasificacion:"+18",
+        idioma:"ES"
+    },
+    {
+        nombre:"El Teléfono Negro ",
+        genero:"Suspenso",
+        duracion: 130,
+        imagenes:"https://firebasestorage.googleapis.com/v0/b/cinedk-77f65.appspot.com/o/TELEFONO%20NEGRO.png?alt=media&token=8b4690d8-4b04-4d9a-a32d-d2a6d097aa10",
+        sinopsis:"Finney Shaw es un niño tímido pero inteligente de 13 años que es secuestrado y atrapado en un sótano insonorizado por un asesino sádico. Cuando un teléfono desconectado en la pared comienza a sonar, Finney descubre que puede escuchar las voces de las víctimas anteriores del asesino. Y están decididos a asegurarse de que lo que les pasó a ellos no le pase a Finney.",
+        clasificacion:"+18",
+        idioma:"EN"
+    }
+]
+ 
+// RECORRIENDO UN ARREGLO EN JS
+
+peliculas.forEach(function(pelicula){
+    console.log(peliculas)
+    console.log(peliculas.nombre)
+    console.log(peliculas.genero)
+    console.log(peliculas.duracion)
+    console.log(peliculas.imagenes)
+    console.log(peliculas.sinopsis)
+    console.log(peliculas.clasificacion)
+    console.log(peliculas.idioma)
+})
